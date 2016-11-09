6398036502c457911063fb50766443d308f9d6b0
Rinto Jose <rinto.jose@tcs.com>
2016-11-10 03:50:27 +0530

    Updating dist

--- dist/virtual-scroll.js
+++ dist/virtual-scroll.js
@@ -15,8 +15,8 @@ var VirtualScrollComponent = (function () {
         this.element = element;
         this.renderer = renderer;
         this.items = [];
-        this.marginX = 0;
-        this.marginY = 0;
+        this.scrollbarWidth = 10;
+        this.scrollbarHeight = 0;
         this.update = new core_1.EventEmitter();
         this.indexUpdate = new core_1.EventEmitter();
         this.startupLoop = true;
@@ -38,10 +38,15 @@ var VirtualScrollComponent = (function () {
         if (index < 0 || index >= (this.items || []).length)
             return;
         var el = this.element.nativeElement;
+        var content = this.contentElementRef.nativeElement;
         var viewWidth = el.clientWidth;
         var viewHeight = el.clientHeight;
-        var childWidth = ((el.children[1] || {}).clientWidth || viewWidth) + (this.marginX * 2);
-        var childHeight = ((el.children[1] || {}).clientHeight || viewHeight) + (this.marginY * 2);
+        var contentDimensions = content.children[0] ? content.children[0].getBoundingClientRect() : {
+            width: viewWidth,
+            height: viewHeight
+        };
+        var childWidth = contentDimensions.width;
+        var childHeight = contentDimensions.height;
         var itemsPerRow = Math.max(1, Math.floor(viewWidth / childWidth));
         var itemsPerCol = Math.max(1, Math.floor(viewHeight / childHeight));
         el.scrollTop = Math.floor(index / itemsPerRow) * childHeight - Math.max(0, (itemsPerCol - 1)) * childHeight;
@@ -49,25 +54,33 @@ var VirtualScrollComponent = (function () {
     };
     VirtualScrollComponent.prototype.calculateItems = function () {
         var el = this.element.nativeElement;
+        var content = this.contentElementRef.nativeElement;
         var scrollTop = el.scrollTop;
         var items = this.items || [];
         var itemCount = items.length;
-        var viewWidth = el.clientWidth;
-        var viewHeight = el.clientHeight;
-        var childWidth = ((el.children[1] || {}).clientWidth || viewWidth) + (this.marginX * 2);
-        var childHeight = ((el.children[1] || {}).clientHeight || viewHeight) + (this.marginY * 2);
+        var viewWidth = el.clientWidth - this.scrollbarWidth;
+        var viewHeight = el.clientHeight - this.scrollbarHeight;
+        var contentDimensions;
+        if (this.childWidth == undefined || this.childHeight == undefined) {
+            contentDimensions = content.children[0] ? content.children[0].getBoundingClientRect() : {
+                width: viewWidth,
+                height: viewHeight
+            };
+        }
+        var childWidth = this.childWidth || contentDimensions.width;
+        var childHeight = this.childHeight || contentDimensions.height;
         var itemsPerRow = Math.max(1, Math.floor(viewWidth / childWidth));
         var itemsPerCol = Math.max(1, Math.floor(viewHeight / childHeight));
-        var scrollHeight = childHeight * itemCount / itemsPerRow;
-        var start = Math.floor(scrollTop / scrollHeight * itemCount / itemsPerRow) * itemsPerRow;
-        var end = Math.min(itemCount, Math.ceil(scrollTop / scrollHeight * itemCount / itemsPerRow) * itemsPerRow + itemsPerRow * (itemsPerCol + 1));
+        this.scrollHeight = childHeight * itemCount / itemsPerRow;
+        var start = Math.floor(scrollTop / this.scrollHeight * itemCount / itemsPerRow) * itemsPerRow;
+        var end = Math.min(itemCount, Math.ceil(scrollTop / this.scrollHeight * itemCount / itemsPerRow) * itemsPerRow + itemsPerRow * (itemsPerCol + 1));
+        ;
         this.topPadding = childHeight * Math.ceil(start / itemsPerRow);
-        this.bottomPadding = childHeight * Math.ceil((itemCount - end) / itemsPerRow);
         if (start !== this.previousStart || end !== this.previousEnd) {
             this.update.emit(items.slice(start, end));
             this.indexUpdate.emit({
                 start: start,
-                end: Math.min(itemCount, end)
+                end: end
             });
             this.previousStart = start;
             this.previousEnd = end;
@@ -86,11 +99,19 @@ var VirtualScrollComponent = (function () {
     __decorate([
         core_1.Input(), 
         __metadata('design:type', Number)
-    ], VirtualScrollComponent.prototype, "marginX", void 0);
+    ], VirtualScrollComponent.prototype, "scrollbarWidth", void 0);
     __decorate([
         core_1.Input(), 
         __metadata('design:type', Number)
-    ], VirtualScrollComponent.prototype, "marginY", void 0);
+    ], VirtualScrollComponent.prototype, "scrollbarHeight", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Number)
+    ], VirtualScrollComponent.prototype, "childWidth", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Number)
+    ], VirtualScrollComponent.prototype, "childHeight", void 0);
     __decorate([
         core_1.Output(), 
         __metadata('design:type', core_1.EventEmitter)
@@ -99,10 +120,15 @@ var VirtualScrollComponent = (function () {
         core_1.Output(), 
         __metadata('design:type', core_1.EventEmitter)
     ], VirtualScrollComponent.prototype, "indexUpdate", void 0);
+    __decorate([
+        core_1.ViewChild('content', { read: core_1.ElementRef }), 
+        __metadata('design:type', core_1.ElementRef)
+    ], VirtualScrollComponent.prototype, "contentElementRef", void 0);
     VirtualScrollComponent = __decorate([
         core_1.Component({
             selector: 'virtual-scroll',
-            template: "\n        <div class=\"padding-layer\" [style.height]=\"topPadding + 'px'\"></div>\n        <ng-content></ng-content>\n        <div class=\"padding-layer\" [style.height]=\"bottomPadding + 'px'\"></div>\n    "
+            template: "\n        <div class=\"total-padding\" [style.height]=\"scrollHeight + 'px'\"></div>\n        <div class=\"scrollable-content\" #content [style.transform]=\"'translateY(' + topPadding + 'px)'\">\n            <ng-content></ng-content>\n        </div>\n    ",
+            styles: ["\n        :host {\n            overflow: hidden;\n            overflow-y: auto;\n            position: relative;\n        }\n        .scrollable-content {\n            top: 0;\n            left: 0;\n            width: 100%;\n            height: 100%;\n            position: absolute;\n        }\n        .total-padding {\n            width: 1px;\n            opacity: 0;\n        }\n    "]
         }), 
         __metadata('design:paramtypes', [core_1.ElementRef, core_1.Renderer])
     ], VirtualScrollComponent);
