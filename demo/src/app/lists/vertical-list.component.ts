7c279937cce58ddd99ebfd43a29cbb54a08220ce
Rinto Jose <rinto.jose@tcs.com>
2017-05-14 14:04:00 +0530

    add sort

--- demo/src/app/lists/vertical-list.component.ts
+++ demo/src/app/lists/vertical-list.component.ts
@@ -5,25 +5,27 @@ import { ListItem } from './list-item.component';
 @Component({
   selector: 'vertical-list',
   template: `
-        <button (click)="reduceList()">Reduce to 100 Items</button>
-        <button (click)="setToFullList()">Revert to 1000 Items</button>
+    <button (click)="sortByName()">Sort By Name</button>
+    <button (click)="sortByIndex()">Sort By Index</button>
+    <button (click)="reduceList()">Reduce to 100 Items</button>
+    <button (click)="setToFullList()">Revert to 1000 Items</button>
 
-        <div class="status">
-             Showing <span class="badge">{{indices?.start + 1}}</span>
-             - <span class="badge">{{indices?.end}}</span>
-             of <span class="badge">{{filteredList?.length}}</span>
-            <span>({{scrollItems?.length}} nodes)</span>
-            </div>
+    <div class="status">
+        Showing <span class="badge">{{indices?.start + 1}}</span>
+        - <span class="badge">{{indices?.end}}</span>
+        of <span class="badge">{{filteredList?.length}}</span>
+      <span>({{scrollItems?.length}} nodes)</span>
+      </div>
 
-        <virtual-scroll
-            [items]="filteredList"
-            (update)="scrollItems = $event"
-            (change)="indices = $event">
+    <virtual-scroll
+      [items]="filteredList"
+      (update)="scrollItems = $event"
+      (change)="indices = $event">
 
-            <list-item *ngFor="let item of scrollItems" [item]="item"> </list-item>
+      <list-item *ngFor="let item of scrollItems" [item]="item"> </list-item>
 
-        </virtual-scroll>
-    `
+    </virtual-scroll>
+  `
 })
 export class VerticalListComponent implements OnChanges {
 
@@ -35,6 +37,14 @@ export class VerticalListComponent implements OnChanges {
 
   reduceList() {
     this.filteredList = (this.items || []).slice(0, 100);
+  }
+
+  sortByName() {
+    this.filteredList = [].concat(this.filteredList || []).sort((a, b) => -(a.name < b.name) || +(a.name !== b.name));
+  }
+
+  sortByIndex() {
+    this.filteredList = [].concat(this.filteredList || []).sort((a, b) => -(a.index < b.index) || +(a.index !== b.index));
   }
 
   setToFullList() {
