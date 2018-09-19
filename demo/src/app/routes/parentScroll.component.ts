dd0d9dee51d766c515e04ac3fa10c665d1a6f8c0
dgarner <devingarner@gmail.com>
2018-09-19 15:59:55 -0600

    update parentScroll demo

--- demo/src/app/routes/parentScroll.component.ts
+++ demo/src/app/routes/parentScroll.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { VirtualScrollComponent } from 'angular2-virtual-scroll';
 import { Http } from '@angular/http';
-import { ListItem } from '../lists/list-item.component';
+import { ListItem, ListItemComponent } from '../lists/list-item.component';
 import { OnInit } from '@angular/core';
 
 @Component({
@@ -14,23 +14,25 @@ import { OnInit } from '@angular/core';
 <button (click)="reduceListToEmpty()">Reduce to 0 Items</button>
 <button (click)="reduceList()">Reduce to 100 Items</button>
 <button (click)="setToFullList()">Revert to 1000 Items</button>
-<button (click)="scrollTo()">Scroll to 50</button>
+<button (click)="scroll.scrollToIndex(50)">Scroll to index 50</button>
+<button (click)="scroll.scrollToPosition(1500)">Scroll to position 1500</button>
 <button (click)="randomHeight = !randomHeight">Toggle Random Height</button>
+<button *ngIf="randomHeight" (click)="ListItemComponent.ResetSeed();">Re-Randomize Item Sizes</button>
+<button *ngIf="randomHeight" (click)="scroll.invalidateAllCachedMeasurements();">Invalidate cached measurements</button>
 
-<div class="status">
-  Showing <span class="badge">{{indices?.start}}</span>
-  - <span class="badge">{{indices?.end}}</span>
-  of <span class="badge">{{filteredList?.length}}</span>
-  <span>({{scrollItems?.length}} nodes)</span>
-</div>
+    <div class="status">
+        Showing <span>{{scroll.viewPortInfo.startIndex}}</span>
+        - <span>{{scroll.viewPortInfo.endIndex}}</span>
+        of <span>{{filteredList?.length}}</span>
+      <span>({{scroll.viewPortItems?.length}} nodes)</span>
+      <span>[scrollStartPosition: {{scroll.viewPortInfo.scrollStartPosition}}px, scrollEndPosition: {{scroll.viewPortInfo.scrollEndPosition}}px, maxScrollPosition: {{scroll.viewPortInfo.maxScrollPosition}}px ]</span>
+    </div>
 
 <virtual-scroll #scroll [parentScroll]="scroll.window"
                 [enableUnequalChildrenSizes]="randomHeight"
-                [items]="filteredList"
-                (update)="scrollItems = $event"
-                (change)="indices = $event">
+                [items]="filteredList">
 
-  <list-item [randomHeight]="randomHeight" *ngFor="let item of scrollItems" [item]="item"> </list-item>
+  <list-item [randomHeight]="randomHeight" *ngFor="let item of scroll.viewPortItems" [item]="item"> </list-item>
 
 </virtual-scroll>
         
@@ -38,44 +40,35 @@ import { OnInit } from '@angular/core';
 })
 export class ParentScrollComponent implements OnInit {
 
-  randomHeight = false;
+  public randomHeight = false;
 
-  items: ListItem[];
-  scrollItems: ListItem[];
-  indices: any;
+  public items: ListItem[];
 
-  filteredList: ListItem[];
+  public filteredList: ListItem[];
 
-  @ViewChild(VirtualScrollComponent)
-  private virtualScroll: VirtualScrollComponent;
-
-  reduceListToEmpty() {
+  public reduceListToEmpty() {
     this.filteredList = [];
   }
 
-  reduceList() {
+  public reduceList() {
     this.filteredList = (this.items || []).slice(0, 100);
   }
 
-  sortByName() {
+  public sortByName() {
     this.filteredList = [].concat(this.filteredList || []).sort((a, b) => -(a.name < b.name) || +(a.name !== b.name));
   }
 
-  sortByIndex() {
+  public sortByIndex() {
     this.filteredList = [].concat(this.filteredList || []).sort((a, b) => -(a.index < b.index) || +(a.index !== b.index));
   }
 
-  setToFullList() {
+  public setToFullList() {
     this.filteredList = (this.items || []).slice();
-  }
-
-  scrollTo() {
-    this.virtualScroll.scrollToIndex(50);
   }
 
   constructor(private http: Http) { }
 
-  ngOnInit() {
+  public ngOnInit() {
     this.http.get('assets/data/items.json')
       .map(response => response.json())
       .subscribe(data => {
