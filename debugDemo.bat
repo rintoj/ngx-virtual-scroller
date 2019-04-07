989cd1fbf121ffc0150c35b0c0f06190506487e4
dgarner <devingarner@gmail.com>
2019-04-06 21:54:15 -0600

    Use ng-packagr to properly format npm publish for Angular consumption (Ivy support)

--- debugDemo.bat
+++
@@ -1,5 +0,0 @@
-cmd /c "npm run build"
-copy /Y dist demo\node_modules\ngx-virtual-scroller\dist
-cd demo
-start "" "cmd /k npm run start"
-cd ..
--- debugDemo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-eval "npm run build"
-cp -R dist/. demo/node_modules/ngx-virtual-scroller/dist
-cd demo
-eval "npm run start"
-cd ..
