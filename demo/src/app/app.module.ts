605476107f75b8ccece53312d613b83829e47a5e
Michał Mrozek <michalmrozek@wp.pl>
2020-09-07 21:09:25 +0200

    feat: support Angular 10

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,38 +1,41 @@
-import { AppComponent } from './app.component';
-import { RouterModule, Router } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { NgModule } from '@angular/core';
+import { AppComponent } from "./app.component";
+import { RouterModule, Router } from "@angular/router";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AboutComponent } from './routes/about.component';
-import { DemoComponent } from './routes/demo.component';
-import { ParentScrollComponent } from './routes/parentScroll.component';
-import { SamplesComponent } from './routes/samples.component';
+import { AboutComponent } from "./routes/about.component";
+import { DemoComponent } from "./routes/demo.component";
+import { ParentScrollComponent } from "./routes/parentScroll.component";
+import { SamplesComponent } from "./routes/samples.component";
 
-import { ListItemComponent } from './lists/list-item.component';
+import { ListItemComponent } from "./lists/list-item.component";
 
-import { HorizontalListComponent } from './lists/horizontal-list.component';
-import { ListWithApiComponent } from './lists/list-with-api.component';
-import { MultiColListComponent } from './lists/multi-col-list.component';
-import { TableListComponent } from './lists/table-list.component';
-import { VerticalListComponent } from './lists/vertical-list.component';
+import { HorizontalListComponent } from "./lists/horizontal-list.component";
+import { ListWithApiComponent } from "./lists/list-with-api.component";
+import { MultiColListComponent } from "./lists/multi-col-list.component";
+import { TableListComponent } from "./lists/table-list.component";
+import { VerticalListComponent } from "./lists/vertical-list.component";
 
-import { VirtualScrollerModule, VirtualScrollerDefaultOptions } from 'ngx-virtual-scroller';
+import {
+  VirtualScrollerModule,
+  VirtualScrollerDefaultOptions,
+} from "ngx-virtual-scroller";
 
 const customDefaultOptions = {
-	scrollThrottlingTime: 0,
-	scrollDebounceTime: 0,
-	scrollAnimationTime: 750,
-	checkResizeInterval: 1000,
-	resizeBypassRefreshThreshold: 5,
-	modifyOverflowStyleOfParentScroll: true,
-	stripedTable: false
+  scrollThrottlingTime: 0,
+  scrollDebounceTime: 0,
+  scrollAnimationTime: 750,
+  checkResizeInterval: 1000,
+  resizeBypassRefreshThreshold: 5,
+  modifyOverflowStyleOfParentScroll: true,
+  stripedTable: false,
 };
 
-//export function defaultOptionsFactory(): VirtualScrollerDefaultOptions {
-//	return customDefaultOptions;
-//}
+// export function defaultOptionsFactory(): VirtualScrollerDefaultOptions {
+// 	return customDefaultOptions;
+// }
 
 @NgModule({
   declarations: [
@@ -49,36 +52,35 @@ const customDefaultOptions = {
     HorizontalListComponent,
   ],
   imports: [
-    RouterModule.forRoot(
-      [
-        { path: 'demo', component: DemoComponent },
-        { path: 'about', component: AboutComponent },
-        { path: 'samples', component: SamplesComponent },
-        { path: 'parentScroll', component: ParentScrollComponent },
-        { path: '**', redirectTo: '/about', pathMatch: 'full' }
-      ]
-    ),
+    RouterModule.forRoot([
+      { path: "demo", component: DemoComponent },
+      { path: "about", component: AboutComponent },
+      { path: "samples", component: SamplesComponent },
+      { path: "parentScroll", component: ParentScrollComponent },
+      { path: "**", redirectTo: "/about", pathMatch: "full" },
+    ]),
     BrowserModule,
     FormsModule,
-    HttpModule,
-    VirtualScrollerModule
+    HttpClientModule,
+    VirtualScrollerModule,
   ],
   providers: [
     {
-		provide: 'virtual-scroller-default-options', useValue: customDefaultOptions
-	}
-	//{
-	//	'virtual-scroller-default-options', useFactory: defaultOptionsFactory
-	//}
+      provide: "virtual-scroller-default-options",
+      useValue: customDefaultOptions,
+    },
+    // {
+    // 	'virtual-scroller-default-options', useFactory: defaultOptionsFactory
+    // }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
   constructor(router: Router) {
     router.events.subscribe(() => {
       setTimeout(() => {
-        window['hljs'].initHighlighting.called = false;
-        window['hljs'].initHighlighting();
+        (window as any).hljs.initHighlighting.called = false;
+        (window as any).hljs.initHighlighting();
       }, 0);
     });
   }
