cce44dc91e236b4074c9fb80755c89916b989545
Mohamed Benmoussa <51405522+medbenmoussa@users.noreply.github.com>
2023-09-19 20:40:10 +0100

    build: bump version

--- demo/src/app/routes/about.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'about',
-  templateUrl: 'about.component.html'
-})
-export class AboutComponent {
-}
--- demo/src/typings.d.ts
+++
@@ -1,8 +0,0 @@
-/* SystemJS module definition */
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
-
-declare var System: any;
-declare module "menuspy";
